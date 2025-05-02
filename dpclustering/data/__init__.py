20ea621e83b7361d6a12c5ae0a8f5808f8bad11b
Mohamed Al-Duraji <mbalduraji@college.harvard.edu>
2025-05-01 22:48:19 -0400

    fix unpack error in experiments by returning data and labels from generate_clusters

--- dpclustering/data/__init__.py
+++ dpclustering/data/__init__.py
@@ -5,16 +5,15 @@ generate_synthetic_data = Data.generate_synthetic_data
 add_noise_to_data = Data.add_noise_to_data
 def generate_clusters(n_clusters=3, points_per_cluster=100, dim=2, cluster_std=0.5, separation=5):
     """
-    Generates synthetic clusters of data points and returns only the data array.
+    Generates synthetic clusters of data points and returns data points and labels.
     """
-    X, _ = Data.generate_clusters(
+    return Data.generate_clusters(
         n_clusters=n_clusters,
         points_per_cluster=points_per_cluster,
         dim=dim,
         cluster_std=cluster_std,
         separation=separation,
     )
-    return X
 plot_clusters = Data.plot_clusters
 clip_rows = Data.clip_rows
 
